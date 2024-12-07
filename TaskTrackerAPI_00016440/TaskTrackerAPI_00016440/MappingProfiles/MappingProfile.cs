13e7f88e4c1cd46b5dcdff647906a7a850536f47
00016640wiut <animemywear@gmail.com>
2024-12-07 04:08:19 +0400

    Initializing API

---
+++ TaskTrackerAPI_00016440/TaskTrackerAPI_00016440/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TaskTrackerAPI_00016440.DTOs;
+using TaskTrackerAPI_00016440.Models;
+
+namespace TaskTrackerAPI_00016440.MappingProfiles
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            // Map Dto to Model and Model to Dto
+            CreateMap<TaskDto, TaskModel>().ReverseMap();
+            CreateMap<UserDto, UserModel>().ReverseMap();
+        }
+    }
+}
---
+++ TaskTrackerAPI_00016440/TaskTrackerAPI_00016440/Migrations/20241206234229_Create User Task.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTrackerAPI_00016440.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserDbSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDbSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskDbSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskDbSet", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskDbSet_UserDbSet_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserDbSet",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskDbSet_UserId",
+                table: "TaskDbSet",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskDbSet");
+
+            migrationBuilder.DropTable(
+                name: "UserDbSet");
+        }
+    }
+}
